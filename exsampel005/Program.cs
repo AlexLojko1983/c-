783c1814464027c7e2ac05ff636e7b5be9321a41
AlexLojko <a_l_g@tut.by>
2023-02-21 21:19:25 +0300

    add mary

---
+++ exsampel005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Add name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "mary")
+{
+    Console.WriteLine("Heppi Mary!!");
+}
+else
+{
+    Console.WriteLine("Sorry dont Mary");
+}
