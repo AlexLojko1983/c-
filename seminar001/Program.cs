2ceb21aa1240fb07b735edfaad1f8d4f391531f7
AlexLojko <a_l_g@tut.by>
2023-02-23 22:09:16 +0300

    task ..

--- seminar001/Program.cs
+++ seminar001/Program.cs
@@ -1,1 +1,41 @@
-﻿
+﻿//Task1
+/*
+int num1 = 25;      //целочисленное
+double num2 = 12.5;   //вещественное
+
+string line = "asdf";   //строка
+char symbol = 'd' ;      //символ
+bool logikal = true;    //булева переменная num1 > num2
+
+
+Console.WriteLine($"awe {num1} and {num2} ghjkl");
+
+Console.Write("Input a number1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());      //конвертируем в инт32
+*/
+//Task2
+/*
+Console.Write("Input a number2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int quad2 = number2 * number2;
+
+if (number1 == quad2)
+    Console.WriteLine("ÿes!");      //Если одна строка то {} не нужны
+else
+    Console.WriteLine("No!!");
+*/
+
+//Task3
+/*
+Console.Write("input a positive number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = -num;
+while (current <= num)
+{
+    Console.Write(current + " ");
+    current++;
+}
+*/
+// Task4
